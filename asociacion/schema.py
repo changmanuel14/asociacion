f0431e06d6587397a9065c6180266a813a5f45e1
changmanuel14 <changmanuel14@gmail.com>
2020-11-24 21:11:33 -0600

    Se incluyeron mutaciones para completar CRUD

--- asociacion/schema.py
+++ asociacion/schema.py
@@ -4,4 +4,7 @@ import asociados.schema
 class Query(asociados.schema.Query, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(asociados.schema.Mutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- asociados/schema.py
+++ asociados/schema.py
@@ -44,3 +44,110 @@ class Query(graphene.ObjectType):
 
     asociado = relay.Node.Field(AsociadoNode)
     all_asociados = DjangoFilterConnectionField(AsociadoNode)
+
+
+class CreateAsociado(graphene.Mutation):
+    class Arguments:
+        nombre = graphene.String()
+        apellido = graphene.String()
+        fecha_nacimiento = graphene.types.datetime.DateTime()
+        fecha_renMem = graphene.types.datetime.DateTime()
+        telefono = graphene.Int()
+        direccion = graphene.String()
+        correo = graphene.String()
+        especialidad = graphene.List(graphene.ID)
+        subespecialidad = graphene.List(graphene.ID)
+
+    asociado = graphene.Field(AsociadoNode)
+
+    def mutate(self, info, nombre=None, apellido=None, fecha_nacimiento=None, fecha_renMem=None, telefono=None, direccion=None, correo=None, especialidad=None, subespecialidad=None):
+        asociado = Asociado.objects.create(
+            nombre=nombre,
+            apellido=apellido,
+            fecha_nacimiento=fecha_nacimiento,
+            fecha_renMem=fecha_renMem,
+            telefono=telefono,
+            direccion=direccion,
+            correo=correo
+        )
+
+        if especialidad is not None:
+            especialidad_set = []
+            for especialidad_id in especialidad:
+                especialidad_object = Especialidad.objects.get(pk=especialidad_id)
+                especialidad_set.append(especialidad_object)
+            asociado.especialidad.set(especialidad_set)
+
+        if subespecialidad is not None:
+            subespecialidad_set = []
+            for subespecialidad_id in subespecialidad:
+                subespecialidad_object = Subespecialidad.objects.get(pk=subespecialidad_id)
+                subespecialidad_set.append(subespecialidad_object)
+            asociado.subespecialidad.set(subespecialidad_set)
+            
+        asociado.save()
+
+        return CreateAsociado(
+            asociado=asociado
+        )
+
+class UpdateAsociado(graphene.Mutation):
+    class Arguments:
+        nombre = graphene.String()
+        apellido = graphene.String()
+        fecha_nacimiento = graphene.types.datetime.DateTime()
+        fecha_renMem = graphene.types.datetime.DateTime()
+        telefono = graphene.Int()
+        direccion = graphene.String()
+        correo = graphene.String()
+        especialidad = graphene.List(graphene.ID)
+        subespecialidad = graphene.List(graphene.ID)
+
+    asociado = graphene.Field(AsociadoNode)
+
+    def mutate(self, info, id, nombre=None, apellido=None, fecha_nacimiento=None, fecha_renMem=None, telefono=None,direccion=None, correo=None, especialidad=None, subespecialidad=None):
+        asociado = Asociado.objects.get(pk=id)
+        asociado.nombre=nombre if nombre is not None else asociado.nombre,
+        asociado.apellido=apellido if apellido is not None else asociado.apellido,
+        asociado.fecha_nacimiento=fecha_nacimiento if fecha_nacimiento is not None else asociado.fecha_nacimiento,
+        asociado.fecha_renMem=fecha_renMem if fecha_renMem is not None else asociado.fecha_renMem,
+        asociado.telefono=telefono if telefono is not None else asociado.telefono,
+        asociado.direccion=direccion if direccion is not None else asociado.direccion,
+        asociado.correo=correo if correo is not None else asociado.correo
+
+        if especialidad is not None:
+            especialidad_set = []
+            for especialidad_id in especialidad:
+                especialidad_object = Especialidad.objects.get(pk=especialidad_id)
+                especialidad_set.append(especialidad_object)
+            asociado.especialidad.set(especialidad_set)
+
+        if subespecialidad is not None:
+            subespecialidad_set = []
+            for subespecialidad_id in subespecialidad:
+                subespecialidad_object = Subespecialidad.objects.get(pk=subespecialidad_id)
+                subespecialidad_set.append(subespecialidad_object)
+            asociado.subespecialidad.set(subespecialidad_set)
+            
+        asociado.save()
+
+        return UpdateAsociado(
+            asociado=asociado
+        )
+
+class DeleteAsociado(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    asociado = graphene.Field(AsociadoNode)
+
+    def mutate(self, info, id):
+        asociado = Asociado.objects.get(pk=id)
+        if asociado is not None:
+            asociado.delete()
+        return DeleteAsociado(asociado=asociado)
+
+class Mutation(graphene.ObjectType):
+    create_asociado = CreateAsociado.Field()
+    update_asociado = UpdateAsociado.Field()
+    delete_asociado = DeleteAsociado.Field() 
